239ef4f56c3d2cef70c14be41ab64341409431ee
Ju-Rien <juliencorriveautrudel@hotmail.com>
2020-02-03 13:25:31 -0500

    Début affichage des arbres de cercles

--- MoebiusTr/circletree.py
+++ MoebiusTr/circletree.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Ellipse
+
 class TreeNode:
     """Trinary tree, which generates at its initialisation a n_layers deep tree of circles, given 4 Moebius Transformations."""
     def __init__(self, circle, last_tr, last_tr_inv, oth_tr, oth_tr_inv, n_layers):
@@ -17,7 +21,7 @@ class TreeNode:
         return TreeIter(self)
 
 
-    def PrintTree(self):
+    def print_tree(self):
         """Prints the tree in order (itself before its descendants.)"""
         print(self.circle)
         if self.kid1:
@@ -27,6 +31,31 @@ class TreeNode:
         if self.kid3:
             self.kid3.PrintTree()
 
+
+def plot_tree(tree):
+    fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+    if isinstance(tree, TreeNode):
+        ells = [Ellipse(xy= (circ.center().real, circ.center().imag),
+                        width=circ.radius() * 2,
+                        height=circ.radius() * 2,
+                        fill=False)
+                for circ in tree]
+        for e in ells:
+            ax.add_patch(e)
+    else:
+        ells = [[Ellipse(xy= (circ.center().real, circ.center().imag),
+                         width=circ.radius() * 2,
+                         height=circ.radius() * 2,
+                         fill=False)
+                 for circ in node] for node in tree]
+        for n in ells:
+            for e in n:
+                ax.add_patch(e)
+
+    ax.set_xlim((-20,20))
+    ax.set_ylim((-20, 20))
+
+    plt.show()
 
 
 class TreeIter:
@@ -43,17 +72,19 @@ class TreeIter:
 
 
     def _it_gen(self, tree):
+        #import pdb; pdb.set_trace()
+
         yield tree.circle
 
-        if tree.kid1 is not None:
+        if tree.kid1:
             for it in tree.kid1:
                 yield it
 
-        if tree.kid2 is not None:
+        if tree.kid2:
             for it in tree.kid2:
                 yield it
 
-        if tree.kid3 is not None:
+        if tree.kid3:
             for it in tree.kid3:
                 yield it
 
--- MoebiusTr/moebtr.py
+++ MoebiusTr/moebtr.py
@@ -280,6 +280,7 @@ Received: {},{} and {}".format(type(p1), type(p2), type(p3))) from None
 
 
     def center(self):
+        # PROBLEM HERE: ça ne renvoit pas les bons centres
         α, β, γ = self.toComplex()
 
         if abs(α) == float("inf") or abs(β) == float("inf") or abs(γ) == float("inf"):
